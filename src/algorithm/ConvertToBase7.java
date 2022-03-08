719257b92314d67d353b256ffd28ed5641bb7245
Chendy <cdyiwhy@gmail.com>
2022-03-08 23:09:07 +0800

    3.8

---
+++ src/algorithm/ConvertToBase7.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+/*给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。
+
+ 
+
+示例 1:
+
+输入: num = 100
+输出: "202"
+示例 2:
+
+输入: num = -7
+输出: "-10"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/base-7
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class ConvertToBase7 {
+
+    public static String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        boolean negative = num < 0;
+        num = Math.abs(num);
+        StringBuilder digits = new StringBuilder();
+        while (num > 0) {
+            digits.append(num % 7);
+            num /= 7;
+        }
+        if (negative) {
+            digits.append("-");
+        }
+        return digits.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToBase7(-7));
+    }
+}
