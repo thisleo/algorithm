9a92f63a7c74d9614f1929a2b2e00b59aadbaad0
Chendy <cdyiwhy@gmail.com>
2022-03-11 23:07:45 +0800

    3.11

---
+++ src/algorithm/Preorder.java
@@ -0,0 +1,53 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+
+n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+
+
+示例 1：
+
+
+
+输入：root = [1,null,3,2,4,null,5,6]
+输出：[1,3,5,6,2,4]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class Preorder {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        for (Node ch : root.children) {
+            helper(ch, res);
+        }
+    }
+}
