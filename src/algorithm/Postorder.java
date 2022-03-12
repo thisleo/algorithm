8448cba8b80e8171f264fa74e647a69625c4b903
Chendy <cdyiwhy@gmail.com>
2022-03-12 23:29:15 +0800

    3.13

---
+++ src/algorithm/Postorder.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*给定一个 n 叉树的根节点 root ，返回 其节点值的 后序遍历 。
+
+n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class Postorder {
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        for (Node ch : root.children) {
+            helper(ch, res);
+        }
+        res.add(root.val);
+    }
+}
+
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
