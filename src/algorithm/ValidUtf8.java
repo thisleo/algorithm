6e7b857084636bf1ffbc0474b58d2058ccb9b538
Chendy <cdyiwhy@gmail.com>
2022-03-13 23:25:21 +0800

    3.15

---
+++ src/algorithm/ValidUtf8.java
@@ -0,0 +1,57 @@
+package algorithm;
+
+/*给定一个表示数据的整数数组 data ，返回它是否为有效的 UTF-8 编码。
+
+UTF-8 中的一个字符可能的长度为 1 到 4 字节，遵循以下的规则：
+
+对于 1 字节 的字符，字节的第一位设为 0 ，后面 7 位为这个符号的 unicode 码。
+对于 n 字节 的字符 (n > 1)，第一个字节的前 n 位都设为1，第 n+1 位设为 0 ，后面字节的前两位一律设为 10 。剩下的没有提及的二进制位，全部为这个符号的 unicode 码。
+这是 UTF-8 编码的工作方式：
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/utf-8-validation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class ValidUtf8 {
+
+    static final int MASK1 = 1 << 7;
+    static final int MASK2 = (1 << 7) + (1 << 6);
+
+    public boolean validUtf8(int[] data) {
+        int m = data.length;
+        int index = 0;
+        while (index < m) {
+            int num = data[index];
+            int n = getBytes(num);
+            if (n < 0 || index + n > m) {
+                return false;
+            }
+            for (int i = 1; i < n; i++) {
+                if (!isValid(data[index + i])) {
+                    return false;
+                }
+            }
+            index += n;
+        }
+        return true;
+    }
+
+    public int getBytes(int num) {
+        if ((num & MASK1) == 0) {
+            return 1;
+        }
+        int n = 0;
+        int mask = MASK1;
+        while ((num & mask) != 0) {
+            n++;
+            if (n > 4) {
+                return -1;
+            }
+            mask >>= 1;
+        }
+        return n >= 2 ? n : -1;
+    }
+
+    public boolean isValid(int num) {
+        return (num & MASK2) == MASK1;
+    }
+}
