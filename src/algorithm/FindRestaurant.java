ae3cc3635bc8d1d85d43a65255d7d1be8ba2a6b2
Chendy <cdyiwhy@gmail.com>
2022-03-14 23:13:34 +0800

    3.15

---
+++ src/algorithm/FindRestaurant.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*假设 Andy 和 Doris 想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+
+你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设答案总是存在。
+
+ 
+
+示例 1:
+
+输入: list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+输出: ["Shogun"]
+解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+示例 2:
+
+输入:list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，list2 = ["KFC", "Shogun", "Burger King"]
+输出: ["Shogun"]
+解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class FindRestaurant {
+
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> index = new HashMap<String, Integer>();
+        for (int i = 0; i < list1.length; i++) {
+            index.put(list1[i], i);
+        }
+
+        List<String> ret = new ArrayList<String>();
+        int indexSum = Integer.MAX_VALUE;
+        for (int i = 0; i < list2.length; i++) {
+            if (index.containsKey(list2[i])) {
+                int j = index.get(list2[i]);
+                if (i + j < indexSum) {
+                    ret.clear();
+                    ret.add(list2[i]);
+                    indexSum = i + j;
+                } else if (i + j == indexSum) {
+                    ret.add(list2[i]);
+                }
+            }
+        }
+        return ret.toArray(new String[ret.size()]);
+    }
+}
