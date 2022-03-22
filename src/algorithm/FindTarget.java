ec9698cc09958ab59a8cef0ae6e35459a5744248
Chendy <cdyiwhy@gmail.com>
2022-03-22 22:13:09 +0800

    3.22

---
+++ src/algorithm/FindTarget.java
@@ -0,0 +1,68 @@
+package algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*给定一个二叉搜索树 root 和一个目标结果 k，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+
+ 
+
+示例 1：
+
+
+输入: root = [5,3,6,2,4,null,7], k = 9
+输出: true
+示例 2：
+
+
+输入: root = [5,3,6,2,4,null,7], k = 28
+输出: false
+ 
+
+提示:
+
+二叉树的节点个数的范围是  [1, 104].
+-104 <= Node.val <= 104
+root 为二叉搜索树
+-105 <= k <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。。*/
+public class FindTarget {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Set<Integer> set = new HashSet<Integer>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+
+}
+
+
